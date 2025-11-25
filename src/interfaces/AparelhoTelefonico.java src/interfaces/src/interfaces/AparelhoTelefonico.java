07efca9bd4ecb2f2876c6dc645d2b02df7ed8663
Juan <juan_sl90@outlook.com>
2025-11-24 23:00:59 -0300

    Create AparelhoTelefonico.java

---
+++ src/interfaces/AparelhoTelefonico.java src/interfaces/src/interfaces/AparelhoTelefonico.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+/**
+ * Define os comportamentos de um aparelho telefônico.
+ */
+public interface AparelhoTelefonico {
+
+    void ligar(String numero);
+
+    void atender();
+
+    void iniciarCorreioVoz();
+}
