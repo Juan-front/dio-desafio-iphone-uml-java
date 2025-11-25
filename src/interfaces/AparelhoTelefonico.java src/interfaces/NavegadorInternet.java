628379605c533bb5d68d5af7c2ee4d67cf9d370b
Juan <juan_sl90@outlook.com>
2025-11-24 23:02:10 -0300

    Update NavegadorInternet.java

--- src/interfaces/AparelhoTelefonico.java src/interfaces/NavegadorInternet.java
+++ src/interfaces/AparelhoTelefonico.java src/interfaces/NavegadorInternet.java
@@ -1,1 +1,13 @@
+package interfaces;
 
+/**
+ * Define os comportamentos de um navegador de internet.
+ */
+public interface NavegadorInternet {
+
+    void exibirPagina(String url);
+
+    void adicionarNovaAba();
+
+    void atualizarPagina();
+}
