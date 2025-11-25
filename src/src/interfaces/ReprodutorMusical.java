eb85db4eec685b79b36f01962104714074e21892
Juan <juan_sl90@outlook.com>
2025-11-24 22:58:24 -0300

    Update ReprodutorMusical.java

--- src/src/interfaces/ReprodutorMusical.java
+++ src/src/interfaces/ReprodutorMusical.java
@@ -1,1 +1,13 @@
+package interfaces;
 
+/**
+ * Define os comportamentos de um reprodutor musical.
+ */
+public interface ReprodutorMusical {
+
+    void tocar();
+
+    void pausar();
+
+    void selecionarMusica(String musica);
+}
